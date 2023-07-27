78da7e714175c52e71f838ed8447f46483f514f6
norestraint <norestraint@protonmail.com>
2023-07-27 18:28:13 -0300

    Revert "feat: create Player component"
    This reverts commit a92fe7e9c57e8eebc9d95fbd1417c3498c225515.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy::prelude::*;
 
-#[derive(Component)]
-pub struct Pig {
-    pub lifetime: Timer,
-}
-
-#[derive(Resource)]
-pub struct Money(pub f32);
-
-
-//TODO: add system for pig wandering and variable lifetime.
 fn main() {
     App::new()
         .add_plugins(
@@ -26,104 +16,43 @@ fn main() {
                 })
                 .build(),
         )
-        .insert_resource(Money(100.0))
         .add_systems(Startup, setup)
-        .add_systems(Update, (character_movement, spawn_pig, pig_lifetime))
+        .add_systems(Update, character_movement)
         .run();
 }
 
-fn pig_lifetime(
-    mut commands: Commands,
-    time: Res<Time>,
-    mut pigs: Query<(Entity, &mut Pig)>,
-    mut money: ResMut<Money>,
-) {
-    for (pig_entity, mut pig) in &mut pigs {
-        pig.lifetime.tick(time.delta());
-
-        if pig.lifetime.finished() {
-            money.0 += 15.0;
-
-            commands.entity(pig_entity).despawn();
-
-            info!("Pig sold for $15! Current balance: ${:?}", money.0);
-        }
-    }
-}
-
-fn spawn_pig(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    input: Res<Input<KeyCode>>,
-    mut money: ResMut<Money>,
-    player: Query<&Transform, With<Player>>,
-) {
-    if !input.just_pressed(KeyCode::Space) {
-        return;
-    }
-
-    let player_transform = player.single();
-
-    if money.0 >= 10.0 {
-        money.0 -= 10.0;
-        info!("Spent $10 on a pig, remaining money: ${:?}", money.0);
-
-        let texture = asset_server.load("pig.png");
-
-        commands.spawn((
-            SpriteBundle {
-                texture,
-                transform: *player_transform,
-                ..default()
-            },
-            Pig {
-                lifetime: Timer::from_seconds(2.0, TimerMode::Once),
-            },
-        ));
-    }
-}
-
 fn character_movement(
-    mut characters: Query<(&mut Transform, &Player)>,
+    mut characters: Query<(&mut Transform, &Sprite)>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    for (mut transform, player) in &mut characters {
-        let movement_amount = player.speed * time.delta_seconds();
-
-        // TODO: solve diagonal move speed difference.
+    for (mut transform, _) in &mut characters {
         if input.pressed(KeyCode::W) {
-            transform.translation.y += movement_amount;
+            transform.translation.y += 100.0 * time.delta_seconds();
         }
         if input.pressed(KeyCode::S) {
-            transform.translation.y -= movement_amount;
+            transform.translation.y -= 100.0 * time.delta_seconds();
         }
         if input.pressed(KeyCode::D) {
-            transform.translation.x += movement_amount;
+            transform.translation.x += 100.0 * time.delta_seconds();
         }
         if input.pressed(KeyCode::A) {
-            transform.translation.x -= movement_amount;
+            transform.translation.x -= 100.0 * time.delta_seconds();
         }
     }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let mut camera = Camera2dBundle::default();
-
-    camera.projection.scaling_mode = ScalingMode::AutoMin {
-        min_width: 256.0,
-        min_height: 144.0,
-    };
-
-    commands.spawn(camera);
+    commands.spawn(Camera2dBundle::default());
 
     let texture = asset_server.load("character.png");
 
-    commands.spawn((
-        SpriteBundle {
-            texture,
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(100.0, 100.0)),
             ..default()
         },
-        Player { speed: 100.0 },
-    ));
+        texture,
+        ..default()
+    });
 }
